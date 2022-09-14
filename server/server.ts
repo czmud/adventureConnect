8ffc09e0f136a179a5b83431144bdda53f4a3f5d
czmud <caleb.zmuda@protonmail.com>
2022-09-14 13:01:32 -0700

    server working. using model with only name field.

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+const port: number = 8000;
+import "./src/config/mongoose.config"
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+import { AllEventRoutes } from './src/routes/event.routes'
+
+AllEventRoutes(app);
+
+const server = app.listen(port, () =>
+    console.log(`Server is locked and loaded on port ${port}!`)
+);
---
+++ server/src/controllers/event.controller.ts
@@ -0,0 +1,35 @@
+import { Event } from "../models/event.model";
+import { Request, Response } from "express";
+
+export const findAllEvents = (req: Request, res: Response) => {
+    Event.find()
+        .then(allEvents => res.json({ events: allEvents }))
+        .catch(err => res.json({message: "Something went wrong", error: err }));
+}
+
+export const findOneEvent = (req: Request, res: Response) => {
+    Event.findOne({ _id: req.params.id})
+        .then(oneEvent => res.json({ event: oneEvent }))
+        .catch(err => res.json({message: "Something went wrong", error: err }));
+}
+
+export const createNewEvent = (req: Request, res: Response) => {
+    console.log("created "+JSON.stringify(req.body));
+    Event.create(req.body)
+        .then(newlyCreatedEvent => res.json({ event: newlyCreatedEvent }))
+        .catch(err => res.status(400).json(err));
+};
+
+export const updateEvent = (req: Request, res: Response) => {
+    console.log("updated "+JSON.stringify(req.body));
+    Event.findOneAndUpdate({_id: req.params.id}, req.body, { new: true, runValidators: true })
+        .then( updatedEvent => res.json({ event: updatedEvent }))
+        .catch( err => res.status(400).json(err));
+};
+
+export const deleteEvent = (req: Request, res: Response) => {
+    console.log("deleted id: "+req.params.id);
+    Event.deleteOne({_id: req.params.id })
+        .then(result => res.json({ result: result }))
+        .catch(err => res.json({ message: "something went wrong", error: err}));
+};
