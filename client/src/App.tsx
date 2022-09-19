cda48d654234bf59e10884c76b59e45c75518b11
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-19 14:43:58 -0700

    Add/Update background images/colors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './views/main';
+import Dashboard from './views/organizerDashboard';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
       </Routes>
     </div>
   );
