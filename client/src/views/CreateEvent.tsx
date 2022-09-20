d8c561dbb3b6c8420b0bd1763f5815f8f49e56fc
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-19 22:39:08 -0700

    Started Event Create Form and Pages.

---
+++ client/src/views/CreateEvent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EventForm from '../components/EventForm'
+
+const CreateEvent = () => {
+    return (<>
+    <EventForm />
+    </>)
+}
+
+export default CreateEvent
