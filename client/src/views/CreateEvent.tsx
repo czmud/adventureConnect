622e88ae84b0795f7247c728e8d3d5b908b07949
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-20 17:31:37 -0700

    Done-> CRUD displays start Problem-> event user mapping

--- client/src/views/CreateEvent.tsx
+++ client/src/views/CreateEvent.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import EventForm from '../components/EventForm'
+import HeaderBar from '../components/HeaderBar'
 
 const CreateEvent = () => {
     return (<>
-    <EventForm />
+    <HeaderBar title='New events' btnTitle='Logout' btnRoute='logout'/>
+    <EventForm title='Create an Event' btn='Create' />
     </>)
 }
 
