c9d384d20e0014d3119766014dac5a63656e44aa
czmud <caleb.zmuda@protonmail.com>
2022-09-22 18:39:24 -0700

    Added events fetch to dashboard and home page

--- client/src/components/buttons/DeleteButton.tsx
+++ client/src/components/buttons/DeleteButton.tsx
@@ -1,19 +1,25 @@
 import axios from 'axios';
 import React from 'react'
 
+interface DeleteButtonProps{
+    _id: string;
+    entityType: string;
+    buttonName: string;
+    successCallback: Function;
+}
 
-const DeleteButton = (props: any) => {
-    const { eventId, buttonName, successCallback} = props;
+const DeleteButton = (props: DeleteButtonProps) => {
+    const { _id, entityType, buttonName, successCallback} = props;
 
 //! ======== Needs Delete Routes==============
-    const deleteEvent = (req: any, res: any) => {
-        axios.delete(''+eventId)
-            .then(res =>  { console.log(res); successCallback() })
-            .catch(err => console.log("There was an error deleting Event", err))
+    const deleteEvent = () => {
+        axios.delete('http://localhost:8000/api/'+entityType+'/delete/'+_id)
+            .then( () => successCallback() )
+            .catch(errors => console.log(errors));
     };
 
     return (
-    <button onClick= {() => deleteEvent }>
+    <button type="button" onClick= { deleteEvent }>
         {buttonName}
     </button>
     )
