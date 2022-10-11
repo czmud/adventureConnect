a4ffb39b914f2981563b622ec072d05443487fe5
czmud <caleb.zmuda@protonmail.com>
2022-10-11 11:38:29 -0700

    reconfigure mongoose mongo options

--- server/src/config/mongoose.config.ts
+++ server/src/config/mongoose.config.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import mongoose from "mongoose";
 const mongoUri: string = process.env.MONGO_URI!;
 
-mongoose.connect(mongoUri)
+mongoose.connect(mongoUri, {retryWrites: true, w: 'majority', authMechanism: 'DEFAULT'})
 	.then(() => console.log("Established a connection to the database"))
 	.catch(err => console.log("Something went wrong when connecting to the database", err));
