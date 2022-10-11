e66f439a8ba5a4bcc0a18d439f097838b7f35b95
czmud <caleb.zmuda@protonmail.com>
2022-10-11 08:16:27 -0700

    configure server environmental variables

--- server/src/config/mongoose.config.ts
+++ server/src/config/mongoose.config.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import mongoose from "mongoose";
+const mongoUri: string = process.env.MONGO_URI!;
 
-mongoose.connect("mongodb://localhost/adventureConnectDB")
+mongoose.connect(mongoUri)
 	.then(() => console.log("Established a connection to the database"))
 	.catch(err => console.log("Something went wrong when connecting to the database", err));
