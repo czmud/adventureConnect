9f43506dac5ea864f3f49d0534a63418966e9912
czmud <caleb.zmuda@protonmail.com>
2022-10-11 14:08:20 -0700

    remove unnecessary mongoose.connect options

--- server/src/config/mongoose.config.ts
+++ server/src/config/mongoose.config.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import mongoose from "mongoose";
 const mongoUri: string = process.env.MONGODB_URI!;
 
-mongoose.connect(mongoUri, {retryWrites: true, w: 'majority', authMechanism: 'DEFAULT'})
+mongoose.connect(mongoUri)
 	.then(() => console.log("Established a connection to the database"))
 	.catch(err => console.log("Something went wrong when connecting to the database", err));
